7608062d60c0e99ba7dae9406aa23d3175519acc
ImHawkeye <167693724+ImHawkeye@users.noreply.github.com>
2025-10-02 16:31:01 -0500

    Added visual studio code folder

---
+++ NumberGuessGame/NumberGuessGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class NumberGuessingGame;
+{
+    static void Main()
+    {
+        Random rand = new Random():
+        bool playing = true;
+
+    do
+    {
+        Console.WriteLine("Choose difficulty: Easy (1-10), Medium (1-50), Hard (1-100)");
+        string difficulty = Console.ReadLine().ToLower();
